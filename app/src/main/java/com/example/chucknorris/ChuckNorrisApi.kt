6f154f01823f970a35ce435b40890c9c3f76c235
Pedro Henrique Freitas <pedromuab@gmail.com>
2019-12-03 18:12:14 -0300

    impl random jokes endpoint

--- app/src/main/java/com/example/chucknorris/ChuckNorrisApi.kt
+++ app/src/main/java/com/example/chucknorris/ChuckNorrisApi.kt
@@ -1,9 +1,14 @@
 package com.example.chucknorris
 
+import com.example.chucknorris.model.Joke
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ChuckNorrisApi {
 
     @GET("/jokes/categories")
     suspend fun getCategories(): List<String>
+
+    @GET("/jokes/random")
+    suspend fun random(@Query("random") random: String? = null): Joke
 }
--- app/src/main/java/com/example/chucknorris/ChuckNorrisRepository.kt
+++ app/src/main/java/com/example/chucknorris/ChuckNorrisRepository.kt
@@ -1,14 +1,22 @@
 package com.example.chucknorris
 
+import com.example.chucknorris.model.Joke
+
 interface ChuckNorrisRepository {
 
     suspend fun getCategory(): List<String>
+
+    suspend fun random(category: String? = null): Joke
 }
 
 class ChuckNorrisRepositoryImpl(private val chuckNorrisApi: ChuckNorrisApi) :
     ChuckNorrisRepository {
     override suspend fun getCategory(): List<String> {
         return chuckNorrisApi.getCategories()
+    }
+
+    override suspend fun random(category: String?): Joke {
+        return chuckNorrisApi.random(category)
     }
 
 }
