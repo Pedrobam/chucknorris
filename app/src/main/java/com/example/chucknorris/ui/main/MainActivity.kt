60bf50319a5c665c6311a184ddd11aceefc61ff8
Pedro Henrique Freitas <pedromuab@gmail.com>
2019-12-27 14:55:55 -0300

    impl actionbar

--- app/src/main/java/com/example/chucknorris/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/chucknorris/ui/main/MainActivity.kt
@@ -2,13 +2,25 @@ package com.example.chucknorris.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.chucknorris.R
 
 class MainActivity : AppCompatActivity() {
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.main_activity)
-	}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        setUpActionBar()
+    }
 
+    private fun setUpActionBar() {
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
 }
